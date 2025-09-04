216683918017e5561e7088abeee6b92d2d7204b3
Natalia <natalia.qek@gmail.com>
2025-09-04 09:53:44 -0500

    Cambios

--- src/main/java/com/example/examen/dto/AuthResponse.java
+++
@@ -1,8 +0,0 @@
-package com.example.examen.dto;
-
-/**
- * Un DTO que representa la respuesta exitosa de un login,
- * conteniendo el token JWT que el cliente debe usar para futuras peticiones.
- */
-public record AuthResponse(String token, Integer usuId) {
-}
